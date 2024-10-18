8e85ed96bf3c78ebfa6527be49c26e7a2a00ee7e
kakimura <kakimura0000kenji@gmail.com>
2024-10-18 14:36:26 +0900

    メール送信テスト途中

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -3,10 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Mail\ContactsSendMail;
 use Illuminate\Http\Request;
 
 class ContactsController extends Controller
 {
+    public function send(Request $request)
+    {
+        // バリデーション
+        $request->validate([
+        'email' => 'required|email',
+        'title' => 'required',
+        'body' => 'required'
+      ]);
+    
+        // actionの値を取得
+        $action = $request->input('action');
+    
+        // action以外のinputの値を取得
+        $inputs = $request->except('action');
+    
+        //actionの値で分岐
+        if($action !== 'submit'){
+    
+            // 戻るボタンの場合リダイレクト処理
+            return redirect()
+            ->route('contact.index')
+            ->withInput($inputs);
+            
+        } else {
+            // 送信ボタンの場合、送信処理
+    
+            // ユーザにメールを送信
+            \Mail::to($inputs['email'])->send(new ContactsSendmail($inputs));
+            // 自分にメールを送信
+            \Mail::to('自分のメールアドレス')->send(new ContactsSendmail($inputs));
+    
+            // 二重送信対策のためトークンを再発行
+            $request->session()->regenerateToken();
+    
+            // 送信完了ページのviewを表示
+            return view('contact.thanks');
+        }
+    }
+
     public function index()
     {
         // 入力ページを表示
@@ -33,4 +73,3 @@ class ContactsController extends Controller
         ]);
     }
 }
-
